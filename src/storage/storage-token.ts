92bdeee09e874de96bf0de68e4c13eeccb9eea5c
Luciano Tavernard <lucianotavernard@gmail.com>
2023-06-26 18:55:14 -0300

    chore: update storage helpers

--- src/storage/storage-token.ts
+++ src/storage/storage-token.ts
@@ -2,14 +2,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { tokenCollection } from '@/storage/storage-config'
 
-export async function getToken() {
-  await AsyncStorage.getItem(tokenCollection)
+export async function storageAuthTokenGet() {
+  return await AsyncStorage.getItem(tokenCollection)
 }
 
-export async function saveToken(token: string) {
+export async function storageAuthTokenSave(token: string) {
   await AsyncStorage.setItem(tokenCollection, token)
 }
 
-export async function deleteToken() {
+export async function storageAuthTokenRemove() {
   await AsyncStorage.removeItem(tokenCollection)
 }
--- src/storage/storage-user.ts
+++ src/storage/storage-user.ts
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { userCollection } from '@/storage/storage-config'
 
-export async function getUser() {
+export async function storageUserGet() {
   const storage = await AsyncStorage.getItem(userCollection)
 
   const user: User = storage ? JSON.parse(storage) : {}
@@ -10,10 +10,10 @@ export async function getUser() {
   return user
 }
 
-export async function saveUser(user: User) {
+export async function storageUserSave(user: User) {
   await AsyncStorage.setItem(userCollection, JSON.stringify(user))
 }
 
-export async function deleteUser() {
+export async function storageUserRemove() {
   await AsyncStorage.removeItem(userCollection)
 }
